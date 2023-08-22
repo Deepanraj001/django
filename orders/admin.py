3324969ea8de7b1da91cf75f8339afa6eb1b5cef
student <Deepanraj001>
2023-08-22 17:33:23 +0530

    first commit

---
+++ orders/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from orders.models import data1
+
+admin.site.register(data1)
+
+
+# Register your models here.
---
+++ shopping/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from shopping.models import data2
+
+# Register your models here.
+admin.site.register(data2)
